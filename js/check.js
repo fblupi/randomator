2af0fe807555cf5b6d472d2d12fb301441158f0e
Francisco Javier Bolívar Lupiáñez <fblupi@correo.ugr.es>
2016-10-18 15:22:55 +0200

    Check people names not void #3

--- js/check.js
+++ js/check.js
@@ -5,11 +5,25 @@
 function checkInitialConfiguration() {
   var peopleNumber = $("#people_number").val();
   var teamNumber = $("#team_number").val();
-  if (peopleNumber != null && teamNumber != null && peopleNumber >= teamNumber) {
+  if (peopleNumber !== '' && teamNumber !== '' && peopleNumber >= teamNumber) {
     createPeopleConfiguration(peopleNumber);
   } else {
     Materialize.toast("People number should be higher or equal than team number", 4000);
   }
+}
+
+/**
+ * Check if every input from people configuration isn't void
+ */
+function checkPeopleConfiguration() {
+  var peopleNumber = $("#people_names").children().length;
+  for (var i = 1; i < peopleNumber; i++) {
+    if ($("#name_" + i) === '') {
+      Materialize.toast("Person name can't be void", 4000);
+      return;
+    }
+  }
+  //createTeams();
 }
 
 /**
@@ -20,7 +34,7 @@ function createPeopleConfiguration(peopleNumber) {
   var result =  '<h2>People configuration</h2>';
       result += '<div class="row">';
       result +=   '<form class="col s12" name="people-configuration-form" role="form" onsubmit="checkPeopleConfiguration();" action="javascript: return false;">';
-      result +=     '<div class="row">';
+      result +=     '<div class="row" id="people_names">';
       for (var i = 1; i <= peopleNumber; i++) {
         result +=     '<div class="input-field col s12 m4">';
         result +=       '<input id="name_' + i + '" type="text" class="validate">';
@@ -32,5 +46,5 @@ function createPeopleConfiguration(peopleNumber) {
       result +=   '</form>';
       result += '</div>';
 
-  $("#people_configuration").append(result);
+  $("#people_configuration").html(result);
 }
