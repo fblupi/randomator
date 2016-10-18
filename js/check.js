0adee93fc82da0b7fde2f5140378231a487ad657
Francisco Javier Bolívar Lupiáñez <fblupi@correo.ugr.es>
2016-10-18 17:32:03 +0200

    Reset button #5

--- js/check.js
+++ js/check.js
@@ -3,9 +3,13 @@
  * action or inform the user of the error
  */
 function checkInitialConfiguration() {
-  var peopleNumber = $("#people_number").val();
-  var teamNumber = $("#team_number").val();
+  var peopleNumberField = $("#people-number");
+  var teamNumberField = $("#team-number");
+  var peopleNumber = peopleNumberField.val();
+  var teamNumber = teamNumberField.val();
   if (peopleNumber !== '' && teamNumber !== '' && peopleNumber >= teamNumber) {
+    peopleNumberField.attr("disabled", true);
+    teamNumberField.attr("disabled", true);
     createPeopleConfiguration(peopleNumber);
   } else {
     Materialize.toast("People number should be higher or equal than team number", 4000);
@@ -16,35 +20,12 @@ function checkInitialConfiguration() {
  * Check if every input from people configuration isn't void
  */
 function checkPeopleConfiguration() {
-  var peopleNumber = $("#people_names").children().length;
+  var peopleNumber = $("#people-number").val();
   for (var i = 1; i < peopleNumber; i++) {
-    if ($("#name_" + i) === '') {
+    if ($("#name-" + i) === '') {
       Materialize.toast("Person name can't be void", 4000);
       return;
     }
   }
   //createTeams();
-}
-
-/**
- * Generate html fields for introducing people numbers
- * @param {number} peopleNumber - Number of people
- */
-function createPeopleConfiguration(peopleNumber) {
-  var result =  '<h2>People configuration</h2>';
-      result += '<div class="row">';
-      result +=   '<form class="col s12" name="people-configuration-form" role="form" onsubmit="checkPeopleConfiguration();" action="javascript: return false;">';
-      result +=     '<div class="row" id="people_names">';
-      for (var i = 1; i <= peopleNumber; i++) {
-        result +=     '<div class="input-field col s12 m4">';
-        result +=       '<input id="name_' + i + '" type="text" class="validate">';
-        result +=       '<label for="name_' + i + '">Name #' + i + '</label>';
-        result +=     '</div>';
-      }
-      result +=     '</div>';
-      result +=     '<button class="btn waves-effect waves-light" type="submit" name="action">Let&#39;s make teams!</button>';
-      result +=   '</form>';
-      result += '</div>';
-
-  $("#people_configuration").html(result);
 }
---
+++ js/randomator.js
@@ -0,0 +1,36 @@
+/**
+ * Generate html fields for introducing people numbers
+ * @param {number} peopleNumber - Number of people
+ */
+function createPeopleConfiguration(peopleNumber) {
+  var result =  '<h2>People configuration</h2>';
+      result += '<div class="row">';
+      result +=   '<form class="col s12" name="people-configuration-form" role="form" onsubmit="checkPeopleConfiguration();" action="javascript: return false;">';
+      result +=     '<div class="row" id="people-names">';
+      for (var i = 1; i <= peopleNumber; i++) {
+        result +=     '<div class="input-field col s12 m4">';
+        result +=       '<input id="name-' + i + '" type="text" class="validate">';
+        result +=       '<label for="name-' + i + '">Name #' + i + '</label>';
+        result +=     '</div>';
+      }
+      result +=     '</div>';
+      result +=     '<button class="btn waves-effect waves-light" type="submit" name="action">Let&#39;s make teams!</button>';
+      result +=   '</form>';
+      result += '</div>';
+
+  $("#people-configuration").html(result);
+}
+
+/**
+ * Clear results
+ */
+function clearResults() {
+  var peopleNumberField = $("#people-number");
+  var teamNumberField = $("#team-number");
+  var peopleConfiguration = $("#people-configuration");
+  var teamsResult = $("#teams-result");
+  peopleNumberField.removeAttr("disabled");
+  teamNumberField.removeAttr("disabled");
+  peopleConfiguration.html("");
+  teamsResult.html("");
+}
